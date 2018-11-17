a47c9030d46bd6eb9e03a2326016167ffbfc65a8
Frank <i@nyan.im>
2018-11-18 00:33:13 +0800

    update

--- optandconvert.sh
+++ optandconvert.sh
@@ -1,3 +1,3 @@
 python -m tensorflow.python.tools.optimize_for_inference \
 --input=static/models/output_graph.pb --output=optimized_graph.pb \
---input_names="Placeholder" --output_names="final_result"
+--input_names="input" --output_names="final_result"
